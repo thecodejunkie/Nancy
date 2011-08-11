b97a8c630b03efaa6b37de33fc2039a188d6b977
Andreas Hakansson <andreas@selfinflicted.org>
2011-08-12 00:11:23 +0200

    Started adding OAuth

---
+++ src/Nancy.Demo.Authentication.OAuth/OAuthBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿namespace Nancy.Demo.Authentication.OAuth
+{
+    using Nancy.Authentication.OAuth;
+
+    public class OAuthBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void InitialiseInternal(TinyIoC.TinyIoCContainer container)
+        {
+            OAuth.Enable(this, new OAuthConfiguration
+            {
+                Authorization = "/authorize",
+                Base = "/oauth"
+            });
+
+            base.InitialiseInternal(container);
+        }
+    }
+
+    public class RootModule : NancyModule
+    {
+        public RootModule()
+        {
+            Get["/"] = parameters => {
+                return "This is the root";
+            };
+        }
+    }
+}
