cce1396212b1f8b1ffd0907a0e84bfc33bcf83f4
Andreas Hakansson <andreas@selfinflicted.org>
2011-08-26 11:12:05 +0200

    Added hook to persist token and state

--- src/Nancy.Authentication.OAuth/OAuthAuthenticationModule.cs
+++ src/Nancy.Authentication.OAuth/OAuthAuthenticationModule.cs
@@ -1,12 +1,14 @@
 ﻿namespace Nancy.Authentication.OAuth
 {
+    using System;
     using ModelBinding;
 
     public class OAuthAuthenticationModule : NancyModule
     {
         public OAuthAuthenticationModule(
             IAuthenticationProvider authenticationProvider,
-            IAccessTokenGenerator accessTokenGenerator) : base(OAuth.Configuration.Base)
+            IAccessTokenGenerator accessTokenGenerator,
+            IAccessTokenPersister accessTokenPersister) : base(OAuth.Configuration.Base)
         {
             //this.RequiresAuthentication();
 
@@ -23,11 +25,29 @@
                 var oauth_token =
                     accessTokenGenerator.Generate();
 
+                var cookie =
+                    OAuth.DecryptAndValidateCookie<Authorization>(Request, OAuth.Configuration);
+
+                accessTokenPersister.Persist(oauth_token, cookie.State);
+
                 var target =
                     string.Concat(authentication.Redirect_Uri, "?oauth_token=", oauth_token);
 
                 return Response.AsRedirect(target);
             };
+        }
+    }
+
+    public interface IAccessTokenPersister
+    {
+        void Persist(string accessToken, string scope);
+    }
+
+    public class DefaultAccessTokenPersister : IAccessTokenPersister
+    {
+        public void Persist(string accessToken, string scope)
+        {
+            var i = 10;
         }
     }
 }
